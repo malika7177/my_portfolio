0bfaaf3c7312a8716213ffac944d7e076c92e49b
malika7177 <malikadjrv@gmail.com>
2024-12-12 19:19:45 +0500

    color mode done!

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./navbar.css";
+import { useTheme } from "./ThemeContext";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+
+const Navbar = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === "light" ? sun : moon;
+  return (
+    <div>
+      <div className="logo">
+        <span>M</span>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <img
+              src={themeIcon}
+              alt="Color mode icon"
+              onClick={toggleTheme}
+            />
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
