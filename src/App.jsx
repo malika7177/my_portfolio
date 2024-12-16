37519bebcfa4d64525ee2556a444b1d7c84c410b
malika7177 <malikadjrv@gmail.com>
2024-12-16 12:31:18 +0500

    done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Projects from "./components/Projects/Projects";
@@ -13,6 +14,7 @@ function App() {
       <div id="about"><About /></div>
       <div id="projects"><Projects /></div>
       <div id="contact"><Contact /></div>
+      <Footer />
     </div>
   );
 }
